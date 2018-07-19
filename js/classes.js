bc0eceb03e7008b53f88ef965acaa9e6d4050b58
Thomas Harris <lostmt.tom@gmail.com>
2018-07-19 09:52:11 -0700

    feat Creates classes.js file and poplulates it with entity object

---
+++ js/classes.js
@@ -0,0 +1,18 @@
+class Entity { //holds object with inheritence
+  constructor() {
+    this.sprite = 'images/';
+    this.x = 2;
+    this.y = 5;
+  }
+
+  render() {
+    ctx.drawImage(Resources.get(this.sprite), this.x * 101, this.y * 83);
+  }
+}
+
+class Player extends Entity {
+  constructor() {
+    super();
+    this.sprite += 'char-boy.png';
+  }
+}
