0a11ef9e349c8fa8421cf3d6e830cee56fe3a6da
Thomas Harris <lostmt.tom@gmail.com>
2018-07-20 07:41:14 -0700

    feat Creates update(dt) for Enemies so that the bugs move across the screen

--- js/classes.js
+++ js/classes.js
@@ -5,6 +5,11 @@ class Entity { //holds object with inheritence
     this.y = 5;
   }
 
+  update(dt) {
+      this.isOutOfBoundsX = this.x > 5;
+      this.isOutOfBoundsY = this.y < 1;
+    }
+
   render() {
     ctx.drawImage(Resources.get(this.sprite), this.x * 101, this.y * 83);
   }
@@ -16,11 +21,22 @@ class Player extends Entity {
     this.sprite += 'char-boy.png';
   }
 }
+
 class Enemy extends Entity {
   constructor(x, y) {
     super();
     this.sprite += 'enemy-bug.png';
     this.x = x;
     this.y = y;
+  }
+
+  update(dt) { //adds animation so that bugs move then reset
+    super.update();
+    if(this.isOutOfBoundsX) {
+      this.x = -1;
+    }
+    else {
+      this.x += dt;
+    }
   }
 }
