6f194f1743825315e55e6e10a383e8d0c6b416ab
Thomas Harris <lostmt.tom@gmail.com>
2018-07-31 14:10:30 -0700

    style: Formatted through JavaScript Beautifier

--- js/app.js
+++ js/app.js
@@ -1,34 +1,33 @@
-
 const player = new Player();
-const enemyOne = new Enemy(0, 0.8, 100 * (Math.random() * 0.04));
+const enemyOne = new Enemy(0, 0.8, 100 * (Math.random() * 0.04)); //enemyOne through enemyThree run on the three rows of rock
 const enemyTwo = new Enemy(1, 1.8, 100 * (Math.random() * 0.05));
 const enemyThree = new Enemy(2, 2.8, 100 * (Math.random() * 0.06));
-const allEnemies = [];
-  allEnemies.push(enemyOne, enemyTwo, enemyThree);
+const allEnemies = []; //array into which enemies are pushed
+allEnemies.push(enemyOne, enemyTwo, enemyThree);
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
-document.addEventListener('keyup', function(e) {
-    var allowedKeys = {
-        37: 'left',
-        38: 'up',
-        39: 'right',
-        40: 'down'
-    };
+document.addEventListener('keyup', function (e) {
+	var allowedKeys = {
+		37: 'left',
+		38: 'up',
+		39: 'right',
+		40: 'down'
+	};
 
-    player.handleInput(allowedKeys[e.keyCode]);
+	player.handleInput(allowedKeys[e.keyCode]);
 });
 
-function callModal() {
+function callModal() { //modal called when the player wins by making it to the water
 	const modal = document.querySelector('.modal__body');
 	modal.classList.toggle('hide');
 }
 //callModal(); //keeps modal open for testing and styling
-document.querySelector('.modal__close').addEventListener('click', () => {
+document.querySelector('.modal__close').addEventListener('click', () => { //allows for closing of modal without replaying
 	callModal();
 });
 
 document.querySelector('.modal__button').addEventListener('click', () => { //replay button on modal
-  window.location.reload(true);
-  callModal();
+	window.location.reload(true);
+	callModal();
 });
